ae0848d2267a733003368e4d98a7a9c7f0376d4c
Sylvie-C <sylvie.castet@gmail.com>
2023-08-08 10:41:17 +0200

    Petits correctifs (commentaires et lignes inutiles)

--- connect_subscribe/access.php
+++ connect_subscribe/access.php
@@ -11,29 +11,29 @@ my_xss_post() ;
 
 // CONNEXION.PHP PAGE : 
 
-  // Save results for email already in DB or not
-  $query2 = "SELECT user_email FROM users WHERE user_email=:email" ;  
+  // $query1 = select user email in DB if email entered by user in form is in DB
+  $query1 = "SELECT user_email FROM users WHERE user_email=:email" ;  
+
+  $pdo_statement1 = $db -> prepare($query1) ; 
+  $pdo_statement1 -> execute ( [ "email" => $_POST["email"] ] ) ;   // :email => email entered by user (in _POST)
+
+  // $exists = 1 if query1 result not empty (= if email entered by user is in DB)
+  $exists = $pdo_statement1 -> rowCount() ; 
+
+
+  // $query2 = select username and user email from DB if email/password entered by user in form match DB data (unic email)
+  $query2 = "SELECT username,user_email FROM users WHERE user_email=:email AND password=:password" ; // select username for welcome message later on
 
   $pdo_statement2 = $db -> prepare($query2) ; 
-  $pdo_statement2 -> execute ( [ "email" => $_POST["email"] ] ) ;   // :email => email entered by user (in _POST)
+  $pdo_statement2 -> execute ( [ "email" => $_POST["email"]  ,  "password" => $_POST["pwd"] ] ) ; 
 
-  // if email entered = email in DB, $exists > 0
-  $exists = $pdo_statement2 -> rowCount() ; 
-
-
-  // Check if email/password in DB + save results for password match or not
-  $query3 = "SELECT username,user_email FROM users WHERE user_email=:email AND password=:password" ; // select username for welcome message later on
-
-  $pdo_statement3 = $db -> prepare($query3) ; 
-  $pdo_statement3 -> execute ( [ "email" => $_POST["email"]  ,  "password" => $_POST["pwd"] ] ) ; // pwd = id in connexion.php page only
-
-  $array = $pdo_statement3 -> fetch() ; 
-  $_SESSION["username"] = $array["username"] ; // save username from DB in SESSION super global for availability on whole website
-  $_SESSION["email"] = $array["user_email"] ; // save user email from DB in SESSION super global
+  $array = $pdo_statement2 -> fetch() ; 
+  $_SESSION["username"] = $array["username"] ; // save username in SESSION super global for availability on whole website
+  $_SESSION["email"] = $array["user_email"] ; // save user email in SESSION super global
   
 
-  // if password entered = password in DB, $match > 0
-  $match = $pdo_statement3 -> rowCount() ; 
+  // $match=1 if password found in DB
+  $match = $pdo_statement2 -> rowCount() ; 
 
 
 // ACCESS TESTS
